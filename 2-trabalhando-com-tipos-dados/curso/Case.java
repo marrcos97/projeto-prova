19db50162dc9796b0640ae74ac1d09686eeb0878
marrcos97 <marrcos.97@gmail.com>
2018-05-06 18:05:18 -0300

    curso

---
+++ 2-trabalhando-com-tipos-dados/curso/Case.java
@@ -0,0 +1,23 @@
+public class Case{
+	public static void main(String[] args){
+			int teste = 3;
+		switch(teste){
+			default:
+				System.out.println("default");
+				break;
+			case 0:
+				System.out.println("0");
+				break;
+			case 0 + 1:
+				System.out.println("1");
+				break;
+			case 3:
+				System.out.println("3");
+				break;
+		}
+		
+		final int cons;
+		cons = 1;
+		//cons = 2;
+	}
+}
--- 2-trabalhando-com-tipos-dados/curso/Strings.java
+++ 2-trabalhando-com-tipos-dados/curso/Strings.java
@@ -9,5 +9,29 @@ public class Strings{
 		
 		System.out.println("Marcos e " + 10  + 10);
 		System.out.println(10  + 10 + " O Marcos e ");
+		
+		String a1 = new String("Marcos");
+		String a2 = new String("Marcos");
+		System.out.println(a1 == a2);
+		System.out.println(a1.equals(a2));
+		
+		String a3 = "sa";
+		System.out.println(a3.substring(1,2));
+		
+		Carro c1 = new Carro();
+		Carro c2 = new Carro();
+		c1.preco = "1";
+		c2.preco = "2";
+		
+		System.out.println(c1.equals(c2));
+		
+	}
+}
+class Carro{
+	String preco;
+	
+	public boolean equals(Object o){
+		Carro outro = (Carro) o;
+		return this.preco.equals(outro.preco);
 	}
 }
